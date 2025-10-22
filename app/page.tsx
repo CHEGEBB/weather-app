0223e7e3b176b4576adb08f70f78d4d807806c27
CHEGEBB <chegephil24@gmail.com>
2025-10-22 14:55:38 +0300

    Add weather page layout and styles for improved UI presentation

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import UnitsSelector from '@/components/UnitsSelector';
 import Image from 'next/image';
 import React from 'react';
 import { useState } from 'react';
+import '@/sass/weather.scss';
 
 
 function WeatherPage() {
@@ -12,7 +13,8 @@ function WeatherPage() {
     setSelectedValue(event.target.value);
   };
   return (
-    <div className='bg-[#03012d] align-center flex justify-center min-h-screen'>
+    <div className='bg-[#03012d] align-center flex flex-column justify-center min-h-screen'>
+    <div className="main-content">
     <div className="topbar flex flex-row justify-between bg-red-30 min-w-screen h-20 align-center p-8">
       <div className="logo-left">
         <Image
@@ -25,6 +27,71 @@ function WeatherPage() {
       <div className="units-selector">
       <UnitsSelector/>
       </div>
+    </div>
+    <div className="container flex flex-col justify-center align-center min-w-screen ">
+    <div className="content flex flex-col justify-center align-center min-w-screen gap-16 mt-18">
+      <div className="text-sky">
+      <h1 className='text-white text-5xl font-bold text-center'>How&apos;s the sky looking today?</h1>
+      </div>
+    <div className="search flex justify-center align-center gap-5">
+      <input
+        type="text"
+        placeholder="Search for a place..."
+        className="p-4 rounded-lg bg-[#312f4b] w-110 outline-0"
+      />
+      <button className='bg-[#4455da] rounded-lg w-28'>
+        Search 
+      </button>
+      
+    
+    </div>
+    
+    
+    </div>
+    <div className="cards flex flex-row justify-center align-center min-w-screen">
+        <div className="left-cards">
+          <div className="place-card w-[700px] h-[300px]">
+            Berlin, Germany
+
+          </div>
+          <div className="weather-cards flex flex-row gap-3 ">
+            <div className="feels card w-36 h-24 bg-red-400 rounded-lg">
+              <p>
+                Feels Like
+              </p>
+              <h2>18°</h2>
+            </div>
+            <div className="humidity card card w-36 h-24 bg-red-400 rounded-lg">
+              <p>
+                Humidity
+              </p>
+              <h2>46%</h2>
+          </div>
+            <div className="wind card card w-36 h-24 bg-red-400 rounded-lg">
+              <p>
+                Wind Speed
+              </p>
+              <h2>14 km/h</h2>
+            </div>
+            <div className="precipitation-card card w-36 h-24 bg-red-400 rounded-lg">
+              <p>
+                Precipitation
+              </p>
+              <h2>0 mm</h2>
+            </div>
+          </div>
+
+          <div className="forecast-cards"></div>
+        </div>
+        <div className="right-card">
+          <div className="details-card">
+            Weather Details
+          </div>
+
+        </div>
+      </div>
+      </div>
+
     </div>
     </div>
   );
